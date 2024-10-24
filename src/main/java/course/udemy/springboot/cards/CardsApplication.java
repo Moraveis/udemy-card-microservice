6f8895ef5325b39f66ea99d22a7025e6b8b03d3c
Joao Vitor <joaovitormoraveis@gmail.com>
2024-10-24 19:29:16 -0400

    adds custom properties and access to them

--- src/main/java/course/udemy/springboot/cards/CardsApplication.java
+++ src/main/java/course/udemy/springboot/cards/CardsApplication.java
@@ -1,13 +1,43 @@
 package course.udemy.springboot.cards;
 
+import course.udemy.springboot.cards.dto.CardsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards microservice REST API Documentation",
+                description = "Cards microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Joao Vitor",
+                        email = "test@test.com",
+                        url = "localhost"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "localhost"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Cards microservice REST API Documentation",
+                url = "localhost/swagger-ui.html"
+        )
+)
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 public class CardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
 
 }
